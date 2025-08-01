fdfcff1c9536a472d250534961072a2cddc1ac4c
Alexander Unger <91609611+Alex336699@users.noreply.github.com>
2025-08-01 07:05:20 +0000

    Add base path for Cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // <-- Add this line
   server: {
-    allowedHosts: ["8s289t-5173.csb.app"],
+    allowedHosts: ["q37985-5173.csb.app"],
   },
 });
